68ac43becf2ea4d8b14c57c8971dcc2e40477b1d
Diassia <shaw.kayleighemma@gmail.com>
2022-01-09 11:45:28 +0000

    Created a function to fetch data for nearby stops from transport api

--- bus-stops/main.go
+++ bus-stops/main.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type PostcodeData struct {
@@ -17,9 +20,12 @@ type Postcoderesponse struct {
 	Result PostcodeData
 }
 
-func FetchPostcodeCoords(postcode string) (float32, float32) {
+// type Stopsresponse struct {
+// 	Result
+// }
+
+func FetchLocation(postcode string) (float32, float32) {
 	URL := "https://api.postcodes.io/postcodes/" + postcode
-	fmt.Println(URL)
 
 	req, err := http.NewRequest("GET", URL, nil)
 	if err != nil {
@@ -52,8 +58,45 @@ func FetchPostcodeCoords(postcode string) (float32, float32) {
 	return longitude, latitude
 }
 
+func FetchNearbyStops(long float32, lat float32) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// expire 30 days (from 09/01)
+	id := os.Getenv("TRANSPORT_ID")
+	key := os.Getenv("API_KEY")
+
+	URL := "http://transportapi.com/v3/uk/places.json?lat=" + fmt.Sprintf("%f", lat) + "&lon=" + fmt.Sprintf("%f", long) + "&type=bus_stop&app_id=" + id + "&app_key=" + key
+
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := string(bodyBytes)
+	fmt.Println(s)
+
+}
+
 func main() {
-	longitude, latitude := FetchPostcodeCoords("nw51tl")
+	longitude, latitude := FetchLocation("nw51tl")
+	FetchNearbyStops(longitude, latitude)
 
 	fmt.Printf("Longitude: %v\nLatitude: %v\n", longitude, latitude)
 }
